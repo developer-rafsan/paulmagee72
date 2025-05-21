b3d719819c4b7d7f1944edb7a686528af12ef6de
rafsanx <developer.rafsanx@gmail.com>
2025-05-22 00:08:42 +0600

    error fixed

--- api/controllers/getFormData.controller.js
+++ api/controllers/getFormData.controller.js
@@ -2,9 +2,10 @@ import { db } from "../db/dbconnect.js"
 
 export const getFormData = async (req, res, next) => {
     const { id } = req.query
+    console.log(id);
     try {
         const [rows] = await db.execute("SELECT * FROM wp_frmt_form_entry_meta");
-
+        console.log(rows);
         let result = [];
 
         if (!id) {
@@ -12,9 +13,11 @@ export const getFormData = async (req, res, next) => {
         } else {
             result = rows.filter((item) => item.entry_id === Number(id));       
         }
+        console.log(result);
 
         res.json(result);
     } catch (error) {
+        console.log(error);
         res.status(500).json({ error: error.message });
     }
 } 
